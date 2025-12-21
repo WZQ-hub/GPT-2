c814798098a7bd097ff4e0e41f16e8772a5446c1
super <super1wzq1@outlook.com>
2025-12-22 02:12:22 +0800

    MultiHeadAttention

--- Dataloader.py
+++ Dataloader.py
@@ -15,6 +15,7 @@ class GPTDataloaderV1(Dataset):
         self.output_ids = []
         token_ids = tokenizer.encode(data)
 
+        # include the last chunk which may be smaller than max_length
         for i in range(0, len(token_ids) - max_length, stride):
             input_chunk = token_ids[i: i + max_length]
             target_chunk = token_ids[i + 1: i + max_length + 1]
@@ -22,8 +23,22 @@ class GPTDataloaderV1(Dataset):
             self.output_ids.append(torch.tensor(target_chunk))
 
 
-    def __len__(self):
-        return len(self.data)
+    def __len__(self) -> int:
+        return len(self.input_ids)
 
-    def __getstate__(self, idx):
+    def __getitem__(self, idx: int):
         return self.input_ids[idx], self.output_ids[idx]
+
+def create_dataloader_v1(data, batch_size = 4, max_length = 512, stride=128, shuffle=True, drop_last=True,
+        num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDataloaderV1(data, tokenizer, max_length, stride)
+    dataloader = DataLoader(dataset, batch_size = batch_size, shuffle = shuffle, num_workers = num_workers, drop_last = drop_last)
+    return dataloader
+
+# data_loader = create_dataloader_v1(raw_data, batch_size=1, max_length=4, stride=1, shuffle=False)
+# inputs, targets = next(iter(data_loader))
+# print("inputs:", inputs)   # [1, 4]
+# print("targets:", targets) # [1, 4]
+# 验证右移关系：targets 等于 inputs 在时间维上向后对齐一位
+# print(torch.all(inputs[:, 1:] == targets[:, :-1]))  # True
---
+++ model/MultiHeadAttention.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias = False):
+        super().__init__()
+        assert d_out %  num_heads == 0, "d_out must be divisible by num_heads"
+        self.d_in = d_in
+        self.d_out = d_out
+        self.head_dim = d_out // num_heads
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.num_heads = num_heads
+
+        self.dropout = nn.Dropout(dropout)
+        self.proj = nn.Linear(d_out, d_out)
+        self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
+
+    def forward(self, x):
+        batch_size, num_tokens, d_in = x.shape
+        keys = self.W_key(x) # batch_size, num_tokens, d_out
+        queries = self.W_query(x) # batch_size, num_tokens, d_out
+        values = self.W_value(x) # batch_size, num_tokens, d_out
+
+        # 切分为多头
+        keys = keys.view(batch_size, num_tokens, self.num_heads, self.head_dim) # batch_size, num_tokens, num_heads, head_dim
+        queries = queries.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(batch_size, num_tokens, self.num_heads, self.head_dim)
+
+        keys = keys.transpose(1, 2)  # (b,num_heads,num_tokens,head_dim)
+        queries = queries.transpose(1, 2)  # (b,num_heads,num_tokens,head_dim)
+        values = values.transpose(1, 2)  # (b,num_heads,num_tokens,head_dim)
+
+        # attention_scores
+        attention_scores = queries @ keys.transpose(2,3) # batch_size, num_heads, num_tokens, num_tokens
+
+        # masked fill
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+        attention_scores = attention_scores.masked_fill(mask_bool, -float('inf'))
+
+        # attention_weight
+        attention_weights = torch.softmax(attention_scores / keys.shape[-1]**0.5, dim=-1) # batch_size, num_heads, num_tokens, num_tokens
+        attention_weights = self.dropout(attention_weights)
+        # context_vec
+        context_vec = (attention_weights @ values).transpose(1,2) # batch_size, num_tokens, num_heads, head_dim
+        context_vec = context_vec.contiguous().view(
+            batch_size, num_tokens, self.d_out
+        )
+
+        output = self.proj(context_vec)
+        return output
+
+inputs = torch.tensor(
+[[0.43, 0.15, 0.89], # Your (x^1)
+[0.55, 0.87, 0.66], # journey (x^2)
+[0.57, 0.85, 0.64], # starts (x^3)
+[0.22, 0.58, 0.33], # with (x^4)
+[0.77, 0.25, 0.10], # one (x^5)
+[0.05, 0.80, 0.55]] # step (x^6)
+)
+batch = torch.stack((inputs, inputs), dim=0)
+torch.manual_seed(123)
+batch_size, context_length, d_in = batch.shape
+d_out = 2
+mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
+context_vecs = mha(batch)
+print(context_vecs)
+print("context_vecs.shape:", context_vecs.shape)
