2f9c18c5a050a2f8509a3a1374abb485b0557b4b
super <super1wzq1@outlook.com>
2025-12-22 23:36:15 +0800

    GPT-2 model
    Signed-off-by: super <super1wzq1@outlook.com>

---
+++ model/GPT-2.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+from model.Transformer import Transformer
+
+GPT_CONFIG_124M = {
+"vocab_size": 50257, # Vocabulary size
+"context_length": 1024, # Context length
+"emb_dim": 768, # Embedding dimension
+"n_heads": 12, # Number of attention heads
+"n_layers": 12, # Number of layers
+"drop_rate": 0.1, # Dropout rate
+"qkv_bias": False # Query-Key-Value bias
+}
+
+
+class GPT2(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.token_embedding = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.position_embedding = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.dropout = nn.Dropout(cfg["drop_rate"])
+        self.transformer_blocks  =nn.ModuleList([
+            Transformer(cfg) for _ in range(cfg["n_layers"])
+        ])
+        self.layer_norm = nn.LayerNorm(cfg["emb_dim"])
+        self.proj = nn.Linear(cfg["emb_dim"], cfg["vocab_size"])
+
+    def forward(self, x):
+        batch_size, sqe_len = x.shape
+        token_emd = self.token_embedding(x)
+        position_emd = self.position_embedding(torch.arange(0, sqe_len, device=x.device))
+        x = token_emd + position_emd
+        x = self.dropout(x)
+        for block in self.transformer_blocks:
+            x = block(x)
+        x = self.layer_norm(x)
+        x = self.proj(x)
+        return x
--- model/MultiHeadAttention.py
+++ model/MultiHeadAttention.py
@@ -51,19 +51,19 @@ class MultiHeadAttention(nn.Module):
         output = self.proj(context_vec)
         return output
 
-inputs = torch.tensor(
-[[0.43, 0.15, 0.89], # Your (x^1)
-[0.55, 0.87, 0.66], # journey (x^2)
-[0.57, 0.85, 0.64], # starts (x^3)
-[0.22, 0.58, 0.33], # with (x^4)
-[0.77, 0.25, 0.10], # one (x^5)
-[0.05, 0.80, 0.55]] # step (x^6)
-)
-batch = torch.stack((inputs, inputs), dim=0)
-torch.manual_seed(123)
-batch_size, context_length, d_in = batch.shape
-d_out = 2
-mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
-context_vecs = mha(batch)
-print(context_vecs)
-print("context_vecs.shape:", context_vecs.shape)
+# inputs = torch.tensor(
+# [[0.43, 0.15, 0.89], # Your (x^1)
+# [0.55, 0.87, 0.66], # journey (x^2)
+# [0.57, 0.85, 0.64], # starts (x^3)
+# [0.22, 0.58, 0.33], # with (x^4)
+# [0.77, 0.25, 0.10], # one (x^5)
+# [0.05, 0.80, 0.55]] # step (x^6)
+# )
+# batch = torch.stack((inputs, inputs), dim=0)
+# torch.manual_seed(123)
+# batch_size, context_length, d_in = batch.shape
+# d_out = 2
+# mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
+# context_vecs = mha(batch)
+# print(context_vecs)
+# print("context_vecs.shape:", context_vecs.shape)shape
